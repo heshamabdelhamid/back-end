30c13e2d12b1549c766ff8efd566dd8f3bd7accf
heshamabdelhamid <heshamabdelhamid432@gamil.com>
2022-05-28 07:07:35 -0700

    "edit"

--- PHP-BASICS/sessions.php
+++ PHP-BASICS/sessions.php
@@ -1,11 +1,21 @@
 <?php
-
+$path = dirname( realpath(__FILE__)) . DIRECTORY_SEPARATOR . 'SESSIONS';   
+session_get_cookie_params();
+session_set_cookie_params(0);
 session_start();
 
 $_SESSION['id'] = session_create_id();
 $_SESSION['user_name'] = 'hesham';
 
+session_regenerate_id(true);
+session_save_path();    
+session_write_close();
+
+session_name('hesham');
+// var_dump($_SESSION);
+
 $encodesession = session_encode();
+
 
 session_decode($encodesession);
 
